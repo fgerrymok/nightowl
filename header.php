0bfdaa3ec7d24e83fca039a3a2cf71d31901176d
Dongwon Kang <dongwon.dwk@gmail.com>
2024-11-26 15:53:27 -0800

    working on header

--- header.php
+++ header.php
@@ -56,4 +56,27 @@
 			);
 			?>
 		</nav><!-- #site-navigation -->
+
+		<!-- nav for mobile -->
+		<nav id="site-navigation-mobile" class="main-navigation-mobile">
+			<?php 
+			$menu_name = 'mobile menu'; // menu name
+			$menu = wp_get_nav_menu_object($menu_name); // retrive single menu 
+
+			if ($menu) {
+				$menu_items = wp_get_nav_menu_items($menu->term_id); // retrive single menu item
+				
+				if ($menu_items) {
+					foreach ($menu_items as $item) {
+						echo '<li>';
+						echo '<a href="' . esc_url($item->url) . '">' . esc_html($item->title) . '</a>';
+						echo '</li>';
+					}
+				} 
+			}
+			?>
+
+
+		</nav>
+
 	</header><!-- #masthead -->
