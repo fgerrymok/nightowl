0456192700bca91bb3ddb5ddb92b2ddcd5bd560c
Fatemehpn <f.pournassari@gmail.com>
2025-01-10 09:52:25 -0800

    Outputted homepage

--- front-page.php
+++ front-page.php
@@ -18,9 +18,9 @@
                 the_post();
                 get_template_part('template-parts/content', 'page');
         ?>
-        <section class='noc-hero-section'>
+        <!-- Hero Section -->
+        <section class='noc-hero-section home-section'>
             <?php
-                // Featured Section
                 if(have_rows('hero_section')){
                     while (have_rows('hero_section')):
                         the_row();
@@ -28,7 +28,7 @@
                         $hero_description = get_sub_field('hero_short_description');
                         $hero_cta_btn     = get_sub_field('cta_button_link');
                         $hero_image       = get_sub_field('hero_image');
-                        $image_size       = 'medium';    
+                        $image_size       = 'large';    
                     ?>
                     <div class='hero-wrapper'>
                         <div class='hero-info-wrapper'>
@@ -54,15 +54,75 @@
                             endif;
                         ?>
                     </div>
-                    <?php endwhile; ?>
-                    <?php
+                    <?php endwhile;
+                    
                 }
             ?>
         </section>
-
-        <section class='noc-featured-section'>
+        <!-- Featured Section -->
+        <section class='noc-featured-section home-section'>
             <h2>Discover Our Menu</h2>
+            <article class='noc-featured-item item-1'>
+                <img src="<?php echo get_template_directory_uri();?>/assets/hearty-combo.png" alt="Hearty Combo Breakfast">
+                <div class='item-info'>
+                    <h3>Hearty Morning Combo</h3>
+                    <p>A hearty breakfast platter featuring your choice of steak, buttered toast, two sides, and a variety of delicious options to start your day right.</p>
+                </div>
+            </article>
+
+            <article class='noc-featured-item item-2'>
+                <div class='item-info'>
+                    <h3>Meat Lover's Steak Combo</h3>
+                    <p>A hearty combination of tender beef steak, juicy pork chop steak, jumbo cheese sausage, crispy bacon, and a perfectly cooked egg for a satisfying, protein-packed meal.</p>
+                </div>
+                <img src="<?php echo get_template_directory_uri();?>/assets/steak-combo.jpg" alt="Steak Combo">
+            </article>
+
+            <article class='noc-featured-item item-3'>
+                <img src="<?php echo get_template_directory_uri();?>/assets/satay-beef-soup.jpg" alt="Satay Beef Soup Noodle">
+                <div class='item-info'>
+                    <h3>Satay Beef Soup Noodle</h3>
+                    <p>A flavourful noodle soup with tender beef and a rich, aromatic satay broth, offering a perfect blend of spices.</p>
+                </div>
+            </article>
         </section>
+
+        <!-- Order Now Section -->
+        <section class='noc-order-section home-section'>
+            <?php
+                if(have_rows('order_section')){
+                    while(have_rows('order_section')):
+                        the_row();
+                        $order_heading   = get_sub_field('section_heading');
+                        $label_uberEats  = get_sub_field('cta_label_for_uber_eats');
+                        $button_uberEats = get_sub_field('cta_button_for_uber_eats');
+                        $label_doorDash  = get_sub_field('cta_label_for_doordash');
+                        $button_doorDash = get_sub_field('cta_button_for_doordash');
+            ?>
+                <h2>
+                    <!-- Order Now Heading -->
+                    <?php if($order_heading): ?>
+                                <h1><?php echo esc_html($order_heading); ?></h1>
+                    <?php endif; ?>
+                </h2>
+
+                   <!-- UberEats CTA Button -->
+                   <?php if($label_uberEats && $button_uberEats): ?>
+                                <a href='<?php echo esc_url($button_uberEats); ?>'><?php echo esc_html($label_uberEats); ?></a>
+                   <?php endif; ?>
+
+                    <!-- DoorDash CTA Button -->
+                    <?php if($label_doorDash && $button_doorDash): ?>
+                                <a href='<?php echo esc_url($button_doorDash); ?>'><?php echo esc_html($label_doorDash); ?></a>
+                   <?php endif; ?>
+            <?php endwhile; ?>
+            <?php
+                }
+            ?>
+
+        </section>
+
         <?php endwhile; ?>
     </main>
     <?php
+    get_footer();
