5a90a146624a39bcd6d1806f2538254543732838
Dongwon Kang <dongwon.dwk@gmail.com>
2025-01-14 16:05:06 -0800

    fixed minor bug

--- functions.php
+++ functions.php
@@ -143,6 +143,8 @@ function nightowlcafe_scripts() {
 
 	wp_enqueue_script( 'nightowlcafe-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
+	wp_enqueue_script( 'menu-card', get_template_directory_uri() . '/js/menu-card.js', array(), _S_VERSION, true );
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
--- js/navigation.js
+++ js/navigation.js
@@ -123,14 +123,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
-
-// disable a tag for each product
-document.addEventListener("DOMContentLoaded", () => {
-  const productLinks = document.querySelectorAll(".menu-items li");
-
-  productLinks.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-    });
-  });
-});
