e93d48af06e31af304bc7ffbca4672bf14e5f47f
Frazer <fgerrymok@gmail.com>
2025-01-17 12:41:10 -0800

    Removed unused code in js dish rotation file

--- js/spin-on-scroll.js
+++ js/spin-on-scroll.js
@@ -1,24 +1,3 @@
-// const dish = document.querySelectorAll('.item-img');
-
-// dish.forEach((singleDish) => {
-//     const dishImg = singleDish.querySelector('img');
-//     let currentRotation = 0;
-//     let lastScrollY = window.scrollY;
-
-//     window.addEventListener('scroll', () => {
-//         const newScrollY = window.scrollY;
-
-//         if (newScrollY > lastScrollY) {
-//             currentRotation += 3;
-//         } else {
-//             currentRotation -= 3;
-//         }
-
-//         dishImg.style.transform = `rotate(${currentRotation}deg)`;
-//         lastScrollY = newScrollY;
-//     });
-// });
-
 const dish = document.querySelectorAll('.item-img');
 
 dish.forEach((singleDish) => {
